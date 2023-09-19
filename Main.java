0e7afa18f2f0cc24d8f8231b368c4feb713afafd
abinaya <abinaya2872@gmail.com>
2023-09-19 14:30:05 +0530

    New Version

--- Main.java
+++
@@ -1,9 +0,0 @@
-class Main{
-   public static void main(string[])
-   { 
-    System.out.println("Hello world!");
-    System.out.println("Hey Git");
-    System.out.println("i am abi");
-
-   } 
-}
